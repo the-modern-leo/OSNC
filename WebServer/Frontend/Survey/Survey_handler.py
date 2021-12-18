e25ca34ea6ee99217eaa1ea8f98b80f1dfac9405
the-modern-leo <nicholas.a.bradberry@gmail.com>
2021-12-17 17:30:34 -0700

    Added support for Web elements on multiple pages of the GUI

--- WebServer/Frontend/Survey/Survey_handler.py
+++ WebServer/Frontend/Survey/Survey_handler.py
@@ -4,6 +4,11 @@ class SurveyHandler(RequestHandler):
     def get(self):
         self.render("survey.html")
 
+class PowerHandler(RequestHandler):
+    def get(self):
+        self.render("survey_power.html")
+
 handlers = [
     (r"/survey", SurveyHandler),
+    (r"/survey/power", PowerHandler),
 ]
--- WebServer/Frontend/uimodules.py
+++ WebServer/Frontend/uimodules.py
@@ -4,6 +4,10 @@ class HeaderNav(UIModule):
     def render(self):
         return self.render_string("nav.html")
 
+class SideNav(UIModule):
+    def render(self):
+        return self.render_string("sidenav.html")
+
 class HeaderHtml(UIModule):
     def render(self):
         return self.render_string("header.html")
