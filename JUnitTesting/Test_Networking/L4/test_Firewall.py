1eb766790f6507a848f14f55d88a352a9ed39a02
TheModernLeo <nicholas.a.bradberry@gmail.com>
2025-03-10 12:57:03 -0600

    Added support for -Creating L3 interfaces. - batch creating addresses - batch associating addresses to Object Groups

--- JUnitTesting/Test_Networking/L4/test_Firewall.py
+++ JUnitTesting/Test_Networking/L4/test_Firewall.py
@@ -1,32 +1,86 @@
 import unittest
-from Network.L4.firewall import PaloAlto,adddressobj,GroupObj
+from Network.L4.firewall import PaloAlto,adddressobj,GroupObj,Interface
 
 class TestRouter(unittest.TestCase):
     def test_CreateAddressObj(self):
         addr = adddressobj("addr_10.5.0.1","10.5.0.1/32",True)
         result = addr.Create()
-        with PaloAlto(ip="10.7.17.132",pano=True) as p:
+        with PaloAlto(ip="10.2.2.2",pano=True) as p:
             p.SendConfigCommands(result)
 
         pass
 
     def test_CreateGroupObj(self):
-        group = GroupObj("test",True)
+        group = GroupObj("Test",True)
         result = group.Create()
-        with PaloAlto(ip="10.7.17.132",pano=True) as p:
+        with PaloAlto(ip="10.2.2.2",pano=True) as p:
             p.SendConfigCommands(result)
         pass
 
+    def test_CreateBatchAddressObj(self):
+        for x in range(200,231):
+            addr = adddressobj(f"addr_10.2.7.{str(x)}", f"10.2.7.{str(x)}/32", True)
+            result = addr.Create()
+            with PaloAlto(ip="10.2.2.2", pano=True) as p:
+                p.SendConfigCommands(result)
+    def test_batchAssocateAddressToGroupObj(self):
+        results = []
+        for x in range(200,231):
+            group = GroupObj("Test", True)
+            results.append(group.AddAddressObj(f"addr_10.2.7.{str(x)}")[0])
+        with PaloAlto(ip="10.2.2.2", pano=True) as p:
+            p.SendConfigCommands(results)
     def test_AssocateAddressToGroupObj(self):
         group = GroupObj("test",True)
         result = group.AddAddressObj("addr_10.5.0.1")
-        with PaloAlto(ip="10.7.17.132",pano=True) as p:
+        with PaloAlto(ip="10.2.2.2",pano=True) as p:
             p.SendConfigCommands(result)
         pass
 
     def test_DeleteAddressObj(self):
         group = GroupObj("test",True)
         result = group.Delete()
-        with PaloAlto(ip="10.7.17.132",pano=True) as p:
+        with PaloAlto(ip="10.2.2.2",pano=True) as p:
             p.SendConfigCommands(result)
         pass
+
+    def test_CreateL3SubInterfaceOnAgg(self):
+        listOfInterfaces = [
+{"IPAddress":"10.72.0.0/24","Vlan":"2656","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.1.0/24","Vlan":"2706","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.2.0/24","Vlan":"2756","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.3.0/24","Vlan":"2806","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.4.0/24","Vlan":"2856","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.5.0/24","Vlan":"2906","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.6.0/24","Vlan":"2956","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.7.0/24","Vlan":"3006","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.8.0/24","Vlan":"3056","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.9.0/24","Vlan":"3106","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.10.0/24","Vlan":"3156","Location":"TEST1","Router":"FW-test"},
+{"IPAddress":"10.72.11.0/24","Vlan":"3206","Location":"TEST1","Router":"FW-test"},
+]
+        with PaloAlto(ip="10.2.2.2", pano=True,) as p:
+            for inter in listOfInterfaces:
+                I = Interface("ae2",True,template="FW-test",interfacenumber=inter["Vlan"])
+                test = I.Create(inter["IPAddress"],tag="Network",comment=f"{inter['Location']}-Network",VRtoAddTo="Test",AddtoSZ=True,Vsys="vsys1",zone="Network")
+                result = p.SendConfigCommands(test)
+                pass
+
+    def test_DeleteL3SubInterfaceOnAgg(self):
+        listOfInterfaces = [
+            "2656",
+            "2706",
+            "2756",
+            "2806",
+            "2856",
+            "2906",
+            "2956",
+            "3006",
+            "3056",
+            "3106",
+        ]
+        with PaloAlto(ip="10.2.2.2", pano=True) as p:
+            for inter in listOfInterfaces:
+                I = Interface("ae2", True, template="FW-test", interfacenumber=inter)
+                p.SendConfigCommands(I.Delete())
+                pass
--- Network/L4/firewall.py
+++ Network/L4/firewall.py
@@ -33,10 +33,15 @@ class PaloAlto():
         results.append((self.conn.send_command("config t"), "config t"))
         for command in commands:
             results.append((self.conn.send_command(command), command))
+        return results
+    def Commit(self):
+        results = []
         results.append((self.conn.send_command("save config"), "save config"))
+        return results
+    def Save(self):
+        results = []
         results.append((self.conn.send_command("commit"), "commit"))
         return results
-
 class adddressobj():
     def __init__(self,address_name, address,pano):
         """
@@ -142,4 +147,119 @@ class GroupObj():
             clicomands.append(f"delete shared address-group {self.group_name} static {address_name}")
         else:
             clicomands.append(f"delete address-group {self.group_name} static {address_name}")
+        return clicomands
+
+class Interface():
+    def __init__(self,interfacename,pano,template=None,interfacenumber=None):
+        """
+
+        @param interfacename:
+        @type interfacename: str
+        @param pano:
+        @type pano: bool
+        """
+        self.interfacename = interfacename
+        self.pano = pano
+        self.template = template
+        self.interfacenumber = interfacenumber
+    def __str__(self):
+        return str(self.interfacename)
+    def Create(self,ipaddress,tag,comment=None,VRtoAddTo=None,AddtoSZ=None,Vsys=None,zone=None):
+        """
+
+        @return:
+        @rtype: list
+        """
+        clicomands = []
+        basecommand = f"network interface aggregate-ethernet {self.interfacename} layer3 units {self.interfacename}.{self.interfacenumber} tag {self.interfacenumber}"
+        L3command = f"{basecommand} comment {comment} ip {str(ipaddress)}"
+        if self.pano:
+            clicomands.append(f"set template {self.template} config {basecommand}")
+            clicomands.append(f"set template {self.template} config {L3command}")
+            if VRtoAddTo:
+                clicomands.append(f"set template {self.template} config network virtual-router {VRtoAddTo} interface {self.interfacename}.{self.interfacenumber}")
+            if AddtoSZ:
+                clicomands.append(
+                    f"set template {self.template} config vsys {Vsys} import network interface {self.interfacename}.{self.interfacenumber}")
+                clicomands.append(
+                    f"set template {self.template} config vsys {Vsys} zone {zone} network layer3 {self.interfacename}.{self.interfacenumber}")
+        else:
+            clicomands.append(f"set {basecommand}")
+            clicomands.append(f"set {L3command}")
+            if VRtoAddTo:
+                clicomands.append(f"set network virtual-router {VRtoAddTo} interface {self.interfacename}.{self.interfacenumber}")
+            if AddtoSZ:
+                clicomands.append(
+                    f" set zone {zone} network layer3 {self.interfacename}.{self.interfacenumber}")
+
+        return clicomands
+
+    def Delete(self):
+        """
+
+         @return:
+         @rtype: list
+         """
+        clicomands = []
+        basecommand = f"network interface aggregate-ethernet {self.interfacename} layer3 units {self.interfacename}.{self.interfacenumber}"
+        if self.pano:
+            clicomands.append(f"delete template {self.template} config {basecommand}")
+        else:
+            clicomands.append(f"delete {basecommand}")
+        return clicomands
+
+class Zone():
+    def __init__(self,zone_name,pano,vsys=None,template=None):
+        """
+
+        @param zone_name:
+        @type zone_name: str
+        @param pano:
+        @type pano: bool
+        """
+        self.zone_name = zone_name
+        self.pano = pano
+        if pano:
+            if not vsys or not template:
+                print("needs Vsys and template names to be able to configure panorama")
+            self.vsys = vsys
+            self.template = template
+    def __str__(self):
+        return str(self.zone_name)
+    def Create(self):
+        """
+
+        @return:
+        @rtype: list
+        """
+        clicomands = []
+        if self.pano:
+            clicomands.append(f"set template {self.template} config vsys {self.vsys} zone {self.zone_name}")
+        else:
+            clicomands.append(f"set zone {self.zone_name}")
+        return clicomands
+
+    def AddInterfaceToZone(self, Interface):
+        """
+         @return:
+         @rtype: list
+         """
+        clicomands = []
+        if self.pano:
+            clicomands.append(f"set shared address-group {self.zone_name} static {address_name}")
+        else:
+            clicomands.append(f"set address-group {self.zone_name} static {address_name}")
+        return clicomands
+
+    def Delete(self):
+        """
+
+         @return:
+         @rtype: list
+         """
+        clicomands = []
+        if self.pano:
+            clicomands.append(f"delete shared address-group {self.zone_name}")
+        else:
+            clicomands.append(f"delete address - group {self.zone_name}")
         return clicomands
